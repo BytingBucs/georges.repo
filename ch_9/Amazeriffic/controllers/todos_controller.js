41d96cbe8f8fd1092b9ab0d9bbe5340600030f6e
csia-tech <anon@csia.tech>
2016-04-11 15:42:23 +0100

    added http response codes per pg 262, check controllers

--- ch_9/Amazeriffic/controllers/todos_controller.js
+++ ch_9/Amazeriffic/controllers/todos_controller.js
@@ -1,4 +1,3 @@
-//contenue on pg 259
 //creating import modules per ch.9 instructions
 
 // note that this needs to start up one 
@@ -26,6 +25,25 @@ ToDosController.create = function (req, res) {
 	                res.json(200, result);
 	    	}
 	}); 
+};
+
+ToDosController.show = function (req, res) { 
+	// this is the id that gets sent to the URL 
+	var id = req.params.id;
+	ToDo.find({"_id":id}, function (err, todo) { 
+		if (err !== null) {
+                	// we'll return an internal server error
+                	res.json(500, err);
+		}else{
+			if (todo.length > 0) {
+                    	// we'll return success!
+			res.json(200, todo[0]); 
+			}else{
+                    		// we didn't find the todo with that id!
+                    		res.send(404);
+                	}
+		} 
+	});
 };
 
 module.exports = ToDosController;
