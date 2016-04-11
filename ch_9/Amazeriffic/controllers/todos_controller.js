860b53a91082db9fa8909431e03ac762b2dc62e3
csia-tech <anon@csia.tech>
2016-04-11 14:15:30 +0100

    uploading working copy of ch.8 prior to ch.9 modifications

---
+++ ch_9/Amazeriffic/controllers/todos_controller.js
@@ -0,0 +1,29 @@
+// note that this needs to start up one 
+// directory to find our models directory 
+var ToDo = require("../models/todo.js"),
+        ToDosController = {};
+
+ToDosController.index = function (req, res) { 
+	ToDo.find({}, function (err, toDos) {
+            res.json(toDos);
+        });
+};
+
+ToDosController.create = function (req, res) {
+	var newToDo = new ToDo({"description":req.body.description,
+		"tags":req.body.tags});
+	newToDo.save(function (err, result) {
+		console.log(result); 
+		if (err !== null) {
+	                // the element did not get saved!
+	 
+			console.log(err);
+			res.json(500, err); 
+		}else{
+	                res.json(200, result);
+	    	}
+	}); 
+};
+
+module.exports = ToDosController;
+
